873f579407283eb40898b25852469b6691669759
hayleylamberti-arch <hayley.lamberti@gmail.com>
2026-03-17 19:28:26 +0000

    Add QueryClientProvider to fix React Query crash

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "@/hooks/useAuth"; // 👈 ADD THIS
+
+import { AuthProvider } from "@/hooks/useAuth";
+
+// 👇 ADD THESE
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>   {/* 👈 ADD THIS */}
-        <App />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>   {/* 👈 ADD */}
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
