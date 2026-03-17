2403ce689534d7bcbe8b4060d8add60f91ef1de8
hayleylamberti-arch <hayley.lamberti@gmail.com>
2026-03-17 19:25:18 +0000

    Fix AuthProvider wrapping to resolve blank page

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AuthProvider } from "./hooks/useAuth"; // or use-auth depending on your final name
 import App from "./App";
-import "./index.css";
-
-const queryClient = new QueryClient();
+import { AuthProvider } from "@/hooks/useAuth"; // 👈 ADD THIS
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>   {/* 👈 ADD THIS */}
+        <App />
       </AuthProvider>
-    </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
