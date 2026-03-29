6374c1176a2163d5b6d611de1dd43e4a554ba9fc
hayleylamberti-arch <hayley.lamberti@gmail.com>
2026-03-29 15:03:21 +0000

    Fix Brand logo import

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
+import logo from "@/assets/doglife-logo.png";
 
 export default function Brand() {
   return (
-    <Link to="/" className="flex flex-col leading-tight">
-      <span className="text-xl font-semibold text-gray-900 dark:text-white">
-        DogLife
-      </span>
-      <span className="text-xs text-gray-500 dark:text-gray-400">
-        Because they’re family
-      </span>
+    <Link to="/" className="flex items-center space-x-3">
+      
+      {/* LOGO IMAGE */}
+      <img
+        src={logo}
+        alt="DogLife logo"
+        className="h-10 w-10 object-contain"
+      />
+
+      {/* TEXT */}
+      <div className="flex flex-col leading-tight">
+        <span className="text-lg font-semibold text-gray-900">
+          DogLife
+        </span>
+        <span className="text-xs text-gray-500">
+          Because they’re family
+        </span>
+      </div>
+
     </Link>
   );
 }
