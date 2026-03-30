738eecc3ff28c3f2503cb3bc6d38202a8d9a76b1
hayleylamberti-arch <hayley.lamberti@gmail.com>
2026-03-30 20:26:41 +0000

    fix: remove test navbar and restore supplier navbar

--- src/layouts/SupplierLayout.tsx
+++ src/layouts/SupplierLayout.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
+import SupplierNavbar from "@/components/SupplierNavbar";
 
 export default function SupplierLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
 
-      <div style={{ background: "red", padding: 10 }}>
-        TEST NAVBAR
-      </div>
+      {/* ✅ REAL NAVBAR */}
+      <SupplierNavbar />
 
+      {/* ✅ CONTENT BELOW NAVBAR */}
       <div className="pt-20 p-6">
-  <Outlet />
-</div>
+        <Outlet />
+      </div>
 
     </div>
   );
