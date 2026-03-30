167c29595106175564ba5e8bc32604be8fa590bf
hayleylamberti-arch <hayley.lamberti@gmail.com>
2026-03-30 19:45:59 +0000

    fix: add Outlet import to SupplierLayout

--- src/layouts/SupplierLayout.tsx
+++ src/layouts/SupplierLayout.tsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+import SupplierNavbar from "@/components/SupplierNavbar";
+
 export default function SupplierLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
 
-      {/* 🔥 TEMP REMOVE THIS */}
+      {/* 🔥 TEMP: comment this out for debugging */}
       {/* <SupplierNavbar /> */}
 
       <div className="p-6">
