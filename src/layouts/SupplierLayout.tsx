6e9ac4d6ca7c75ed22ec21f5cce8d8f911fecc47
hayleylamberti-arch <hayley.lamberti@gmail.com>
2026-03-30 20:13:00 +0000

    fix: correct RequireAuth usage with allowRoles

--- src/layouts/SupplierLayout.tsx
+++ src/layouts/SupplierLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import SupplierNavbar from "@/components/SupplierNavbar";
 
 export default function SupplierLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
 
-      {/* 🔥 TEMP: comment this out for debugging */}
-      {/* <SupplierNavbar /> */}
+      <div style={{ background: "red", padding: 10 }}>
+        TEST NAVBAR
+      </div>
 
       <div className="p-6">
         <Outlet />
