f33365094e300cba8a28f84666cea5b4410803bf
hayleylamberti-arch <hayley.lamberti@gmail.com>
2026-03-30 18:34:32 +0000

    Fix SupplierLayout missing Outlet

--- src/layouts/SupplierLayout.tsx
+++ src/layouts/SupplierLayout.tsx
@@ -4,13 +4,11 @@ import SupplierNavbar from "@/components/SupplierNavbar";
 export default function SupplierLayout() {
   return (
     <div>
-      {/* NAVBAR */}
       <SupplierNavbar />
 
-      {/* PAGE CONTENT */}
-      <div className="pt-20 px-4">
+      <main className="p-6">
         <Outlet />
-      </div>
+      </main>
     </div>
   );
 }
