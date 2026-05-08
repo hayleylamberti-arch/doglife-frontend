426fe4267a05147bee769fa210e51d777f493261
hayleylamberti-arch <hayley.lamberti@gmail.com>
2026-05-08 15:18:37 +0000

    legal policies

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getMe } from "@/lib/api";
 import RoleNavbar from "@/components/RoleNavbar";
+import Footer from "@/components/Footer";
 
 export default function AppLayout() {
   const {
@@ -14,11 +15,14 @@ export default function AppLayout() {
   });
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex flex-col">
       <RoleNavbar user={user ?? null} />
-      <main className="mx-auto max-w-7xl p-6">
+
+      <main className="mx-auto max-w-7xl p-6 flex-1 w-full">
         {isLoading ? <div className="p-6">Loading...</div> : <Outlet />}
       </main>
+
+      <Footer />
     </div>
   );
 }
